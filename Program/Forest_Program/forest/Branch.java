6e08f4ba8b0a836ac67d9ca863febaf4baf9d2ca
Konyee <konyee.527@gmail.com>
2021-08-06 21:24:43 +0900

    [add] Branch

--- Program/Forest_Program/forest/Branch.java
+++ Program/Forest_Program/forest/Branch.java
@@ -1,5 +1,34 @@
 package forest;
 
-public class Branch {
+/**
+ * Branchクラス
+ */
+public class Branch extends Object {
 
+    private Node parent;
+    private Node child;
+
+    /**
+     * コンストラクタ
+     * @param parent
+     * @param child
+     */
+    Branch(Node parent, Node child) {
+    this.parent = parent;
+    this.child = child;
+    }
+
+    /**
+     * 親ノードを応答する
+     */
+    public Node getParent() {
+    return this.parent;
+    }
+
+    /**
+     * 子ノードを応答する
+     */
+    public Node getChild() {
+    return this.child;
+    }
 }
