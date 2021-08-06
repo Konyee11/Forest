05f7dce9c4a8bf2659ba8322f8bff4fa11fd0fc0
Konyee <konyee.527@gmail.com>
2021-08-06 21:23:58 +0900

    [add] ForestController

--- Program/Forest_Program/forest/ForestController.java
+++ Program/Forest_Program/forest/ForestController.java
@@ -1,5 +1,29 @@
 package forest;
 
-public class ForestController {
+import java.awt.event.MouseEvent;
+import java.awt.Point;
+import mvc.Controller;
 
+/**
+ * ForestControllerクラス
+ */
+public class ForestController extends Controller {
+
+	/**
+	 * コンストラクタ
+	 */
+	public ForestController() {
+		super();
+	}
+
+	/**
+	 * マウスクリックした位置のノードを出力する
+	 */
+	public void mouseClicked(MouseEvent aEvent) {
+  	Point aPoint = aEvent.getPoint();
+		aPoint.translate(view.scrollAmount().x, view.scrollAmount().y);
+		ForestModel aForestModel = (ForestModel)(this.model);
+		aForestModel.mouseClicked(aPoint, aEvent);
+		return;
+	}
 }
